88b7309f75fd1fd81255c610a4136fa1653fcb54
Arsh Anwar <lucifer78908@gmail.com>
2020-12-06 20:28:56 +0530

    Added Categories Screen and Page Route
    Signed-off-by: Arsh Anwar <lucifer78908@gmail.com>

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("The Recipes"),
+        ),
+        body: Center(
+          child: Text('The Recipes For the category'),
+        ));
+  }
+}
