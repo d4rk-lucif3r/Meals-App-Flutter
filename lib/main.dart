340cb4638dd0f4d7955e1c6839d90a05885d3430
Arsh Anwar <lucifer78908@gmail.com>
2020-12-07 20:42:19 +0530

    Added Bottom NavigationBarThemeData
    Signed-off-by: Arsh Anwar <lucifer78908@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'screens/meal_detail_screen.dart';
 
 import 'screens/categories_screen.dart';
 import 'screens/category_meals_screen.dart';
+import 'screens/meal_detail_screen.dart';
+import 'screens/tabs_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,22 +16,56 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Meals App',
+
       theme: ThemeData(
-          primarySwatch: Colors.red,
-          accentColor: Colors.amber,
-          canvasColor: Colors.black,
-          fontFamily: 'Raleway',
-          //pageTransitionsTheme: PageTransitionsTheme(builders: {TargetPlatform.android: CupertinoPageTransitionsBuilder(),}),
-          textTheme: ThemeData.light().textTheme.copyWith(
-                bodyText1: TextStyle(color: Colors.blueGrey),
-                bodyText2: TextStyle(color: Colors.green),
-                headline6: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  fontFamily: 'RobotoCondensed',
-                  fontWeight: FontWeight.bold,
-                ),
-              )),
+        primarySwatch: Colors.red,
+        accentColor: Colors.amber,
+        canvasColor: Colors.black,
+        fontFamily: 'Raleway',
+        //pageTransitionsTheme: PageTransitionsTheme(builders: {TargetPlatform.android: CupertinoPageTransitionsBuilder(),}),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(color: Colors.blueGrey),
+              bodyText2: TextStyle(color: Colors.green),
+              headline6: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+        tabBarTheme: TabBarTheme(
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.grey.withOpacity(.7),
+          labelStyle: TextStyle(
+            fontFamily: 'Raleway',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            backgroundColor: Theme.of(context).primaryColor,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white.withOpacity(.6),
+            elevation: 7,
+            selectedLabelStyle: TextStyle(
+              fontFamily: 'Raleway',
+              fontWeight: FontWeight.bold,
+              fontSize: 12,
+            ),
+            unselectedLabelStyle: TextStyle(
+              fontFamily: 'Raleway',
+              fontWeight: FontWeight.normal,
+              fontSize: 10,
+            ),
+            type: BottomNavigationBarType.shifting),
+        accentColorBrightness: Brightness.dark,
+        cardTheme: CardTheme(
+          color: Colors.white,
+          elevation: 7,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+      ),
 
       //home: CategoriesScreen(),
       initialRoute: '/',
@@ -47,7 +82,7 @@ class MyApp extends StatelessWidget {
         switch (settings.name) {
           case '/':
             return CupertinoPageRoute(
-                builder: (_) => CategoriesScreen(), settings: settings);
+                builder: (_) => TabsScreen(), settings: settings);
           case CategoryMealScreen.routeName:
             return CupertinoPageRoute(
                 builder: (_) => CategoryMealScreen(), settings: settings);
